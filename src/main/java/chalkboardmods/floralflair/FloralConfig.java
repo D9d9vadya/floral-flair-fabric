4befbe63340a79b976220d57f2e3e240a2420f37
Ibex80 <iloewen652@gmail.com>
2021-11-24 19:38:29 -0600

    Fix generation (it was TOO much)

--- src/main/java/chalkboardmods/floralflair/FloralConfig.java
+++ src/main/java/chalkboardmods/floralflair/FloralConfig.java
@@ -7,32 +7,40 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
+
 @Config(name =FloralFlair.MOD_ID)
 public class FloralConfig implements ConfigData {
-    @Comment("The density of Pulse Petal flowers generating in forests [default = 16]")
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    public int pulsePetalDensity = 16;
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    @Comment("The density of Foxnip flowers generating in taigas [default = 16]")
-    public int foxnipDensity = 16;
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    @Comment("Frosted Foxnip flower spawning density in game [default = 16]")
-    public int frostedFoxnipDensity = 16;
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    @Comment("The density of Fairy Blossom flowers generating in flower forests [default = 16]")
-    public int fairyBlossomDensity = 16;
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    @Comment("The density of Gem of the Jungle flowers in jungles [default = 16]")
-    public int jungleGemDensity = 16;
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    @Comment("The density of Rose flowers in forests and plains [default = 16]")
-    public int roseDensity = 16;
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    @Comment("The density of Sunset Poppy flowers in sunflower plains [default = 16]")
-    public int sunsetPoppyDensity = 16;
-    @ConfigEntry.BoundedDiscrete(max = 32L)
-    @Comment("The density of Muscari flowers in snowy places [default = 16]")
-    public int muscariDensity = 16;
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("The density of Pulse Petal flowers generating in forests [default = 8]")
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    public int pulsePetalDensity = 8;
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("The density of Foxnip flowers generating in taigas [default = 8]")
+    public int foxnipDensity = 8;
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("Frosted Foxnip flower spawning density in game [default = 8]")
+    public int frostedFoxnipDensity = 8;
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("The density of Fairy Blossom flowers generating in flower forests [default = 8]")
+    public int fairyBlossomDensity = 8;
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("The density of Gem of the Jungle flowers in jungles [default = 12]")
+    public int jungleGemDensity = 12;
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("The density of Rose flowers in forests and plains [default = 8]")
+    public int roseDensity = 8;
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    @Comment("The density of Muscari flowers in snowy places [default = 8]")
+    public int muscariDensity = 8;
+    @ConfigEntry.BoundedDiscrete(max = 24L)
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("The density of Sunset Poppy flowers in sunflower plains [default = 8]")
+    public int sunsetPoppyDensity = 8;
 
     public static void init() {
         AutoConfig.register(FloralConfig.class, JanksonConfigSerializer::new);
