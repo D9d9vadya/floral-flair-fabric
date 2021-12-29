6d1574027be7e42648cccbb6baceee4db196c76a
Mana Kampman <manastar04@gmail.com>
2021-12-28 17:34:07 -0800

    Fixed flowers not being compostable or flammable

--- src/main/java/chalkboardmods/floralflair/FloralFlair.java
+++ src/main/java/chalkboardmods/floralflair/FloralFlair.java
@@ -3,6 +3,8 @@ package chalkboardmods.floralflair;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.block.*;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.BlockItem;
@@ -84,6 +86,29 @@ public class FloralFlair implements ModInitializer {
 		FloralConfig.init();
 		// Features
 		FloralFeatures.init();
+		// Composting
+		CompostingChanceRegistry.INSTANCE.add(FOXNIP, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(FROSTED_FOXNIP, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(PULSE_PETAL, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(JUNGLE_GEM, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(ROSE, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(SUNSET_POPPY, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(FAIRY_BLOSSOM, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(MUSCARI, 0.65F);
+		CompostingChanceRegistry.INSTANCE.add(PURPUREUM, 0.65F);
+		// Flammability
+		FlammableBlockRegistry.getDefaultInstance().add(FOXNIP, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(FROSTED_FOXNIP, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(PULSE_PETAL, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(JUNGLE_GEM, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(ROSE, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(SUNSET_POPPY, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(FAIRY_BLOSSOM, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(MUSCARI, 100, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(PURPUREUM, 100, 60);
+
+
+
 
 	}
 }
